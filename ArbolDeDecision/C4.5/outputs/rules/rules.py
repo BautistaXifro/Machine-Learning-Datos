dcf3e0b8e429642fa737394235e4156386c5cfd7
BautistaXifro <bauxifro@gmail.com>
2021-01-24 20:24:17 -0300

    separando mejor el set de training y el de test

--- ArbolDeDecision/C4.5/outputs/rules/rules.py
+++ ArbolDeDecision/C4.5/outputs/rules/rules.py
@@ -1,68 +1,192 @@
-def findDecision(obj): #obj[0]: Region, obj[1]: Total_Amount, obj[2]: TRF
-   # {"feature": "TRF", "instances": 16883, "metric_value": 0.9879, "depth": 1}
-   if obj[2]<=2.3358407865900612:
-      # {"feature": "Total_Amount", "instances": 14740, "metric_value": 0.9516, "depth": 2}
-      if obj[1]<=1003967.3752647843:
-         # {"feature": "Region", "instances": 14610, "metric_value": 0.9488, "depth": 3}
+def findDecision(obj): #obj[0]: Region, obj[1]: Total_Amount, obj[2]: Delivery_Year, obj[3]: Delivery_Quarter
+   # {"feature": "Total_Amount", "instances": 15448, "metric_value": 0.9875, "depth": 1}
+   if obj[1]<=1037643.413886339:
+      # {"feature": "Delivery_Year", "instances": 13429, "metric_value": 0.9501, "depth": 2}
+      if obj[2]<=2018:
+         # {"feature": "Region", "instances": 13202, "metric_value": 0.947, "depth": 3}
          if obj[0] == 'Japan':
-            return 'Closed Won'
+            # {"feature": "Delivery_Quarter", "instances": 4600, "metric_value": 0.8967, "depth": 4}
+            if obj[3] == 'Q1':
+               return 'Closed Won'
+            elif obj[3] == 'Q2':
+               return 'Closed Won'
+            elif obj[3] == 'Q4':
+               return 'Closed Won'
+            elif obj[3] == 'Q3':
+               return 'Closed Won'
+            else:
+               return 'Closed Won'
          elif obj[0] == 'EMEA':
-            return 'Closed Won'
+            # {"feature": "Delivery_Quarter", "instances": 3330, "metric_value": 0.9582, "depth": 4}
+            if obj[3] == 'Q2':
+               return 'Closed Won'
+            elif obj[3] == 'Q3':
+               return 'Closed Won'
+            elif obj[3] == 'Q1':
+               return 'Closed Won'
+            elif obj[3] == 'Q4':
+               return 'Closed Won'
+            else:
+               return 'Closed Won'
          elif obj[0] == 'Americas':
-            return 'Closed Won'
+            # {"feature": "Delivery_Quarter", "instances": 2601, "metric_value": 0.9952, "depth": 4}
+            if obj[3] == 'Q3':
+               return 'Closed Lost'
+            elif obj[3] == 'Q2':
+               return 'Closed Won'
+            elif obj[3] == 'Q1':
+               return 'Closed Won'
+            elif obj[3] == 'Q4':
+               return 'Closed Won'
+            else:
+               return 'Closed Won'
          elif obj[0] == 'APAC':
-            return 'Closed Won'
+            # {"feature": "Delivery_Quarter", "instances": 2535, "metric_value": 0.9127, "depth": 4}
+            if obj[3] == 'Q2':
+               return 'Closed Won'
+            elif obj[3] == 'Q3':
+               return 'Closed Won'
+            elif obj[3] == 'Q1':
+               return 'Closed Won'
+            elif obj[3] == 'Q4':
+               return 'Closed Won'
+            else:
+               return 'Closed Won'
          elif obj[0] == 'Middle East':
-            return 'Closed Lost'
+            # {"feature": "Delivery_Quarter", "instances": 136, "metric_value": 0.9231, "depth": 4}
+            if obj[3] == 'Q3':
+               return 'Closed Lost'
+            elif obj[3] == 'Q2':
+               return 'Closed Lost'
+            elif obj[3] == 'Q1':
+               return 'Closed Lost'
+            elif obj[3] == 'Q4':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
          else:
             return 'Closed Lost'
-      elif obj[1]>1003967.3752647843:
-         # {"feature": "Region", "instances": 130, "metric_value": 0.7793, "depth": 3}
-         if obj[0] == 'Americas':
+      elif obj[2]>2018:
+         # {"feature": "Delivery_Quarter", "instances": 227, "metric_value": 0.9689, "depth": 3}
+         if obj[3] == 'Q1':
+            # {"feature": "Region", "instances": 197, "metric_value": 0.9884, "depth": 4}
+            if obj[0] == 'EMEA':
+               return 'Closed Won'
+            elif obj[0] == 'Japan':
+               return 'Closed Lost'
+            elif obj[0] == 'APAC':
+               return 'Closed Lost'
+            elif obj[0] == 'Americas':
+               return 'Closed Lost'
+            elif obj[0] == 'Middle East':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
+         elif obj[3] == 'Q2':
+            # {"feature": "Region", "instances": 21, "metric_value": 0.5917, "depth": 4}
+            if obj[0] == 'Japan':
+               return 'Closed Lost'
+            elif obj[0] == 'Americas':
+               return 'Closed Lost'
+            elif obj[0] == 'APAC':
+               return 'Closed Lost'
+            elif obj[0] == 'EMEA':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
+         elif obj[3] == 'Q4':
             return 'Closed Lost'
-         elif obj[0] == 'EMEA':
-            return 'Closed Lost'
-         elif obj[0] == 'APAC':
-            return 'Closed Lost'
-         elif obj[0] == 'Japan':
-            return 'Closed Lost'
-         elif obj[0] == 'Middle East':
-            return 'Closed Lost'
+         elif obj[3] == 'Q3':
+            # {"feature": "Region", "instances": 4, "metric_value": 0.8113, "depth": 4}
+            if obj[0] == 'Japan':
+               return 'Closed Lost'
+            elif obj[0] == 'Americas':
+               return 'Closed Won'
+            else:
+               return 'Closed Won'
          else:
             return 'Closed Lost'
       else:
          return 'Closed Lost'
-   elif obj[2]>2.3358407865900612:
-      # {"feature": "Region", "instances": 2143, "metric_value": 0.5385, "depth": 2}
+   elif obj[1]>1037643.413886339:
+      # {"feature": "Region", "instances": 2019, "metric_value": 0.5649, "depth": 2}
       if obj[0] == 'Americas':
-         # {"feature": "Total_Amount", "instances": 903, "metric_value": 0.5742, "depth": 3}
-         if obj[1]>1003967.3752647843:
-            return 'Closed Lost'
-         elif obj[1]<=1003967.3752647843:
-            return 'Closed Lost'
+         # {"feature": "Delivery_Year", "instances": 861, "metric_value": 0.6123, "depth": 3}
+         if obj[2]<=2018:
+            # {"feature": "Delivery_Quarter", "instances": 806, "metric_value": 0.5912, "depth": 4}
+            if obj[3] == 'Q3':
+               return 'Closed Lost'
+            elif obj[3] == 'Q2':
+               return 'Closed Lost'
+            elif obj[3] == 'Q1':
+               return 'Closed Lost'
+            elif obj[3] == 'Q4':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
+         elif obj[2]>2018:
+            # {"feature": "Delivery_Quarter", "instances": 55, "metric_value": 0.8454, "depth": 4}
+            if obj[3] == 'Q1':
+               return 'Closed Lost'
+            elif obj[3] == 'Q2':
+               return 'Closed Lost'
+            elif obj[3] == 'Q3':
+               return 'Closed Won'
+            else:
+               return 'Closed Won'
          else:
             return 'Closed Lost'
       elif obj[0] == 'EMEA':
-         # {"feature": "Total_Amount", "instances": 733, "metric_value": 0.5856, "depth": 3}
-         if obj[1]>1003967.3752647843:
-            return 'Closed Lost'
-         elif obj[1]<=1003967.3752647843:
+         # {"feature": "Delivery_Year", "instances": 699, "metric_value": 0.5995, "depth": 3}
+         if obj[2]<=2018:
+            # {"feature": "Delivery_Quarter", "instances": 667, "metric_value": 0.6171, "depth": 4}
+            if obj[3] == 'Q2':
+               return 'Closed Lost'
+            elif obj[3] == 'Q3':
+               return 'Closed Lost'
+            elif obj[3] == 'Q1':
+               return 'Closed Lost'
+            elif obj[3] == 'Q4':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
+         elif obj[2]>2018:
             return 'Closed Lost'
          else:
             return 'Closed Lost'
       elif obj[0] == 'APAC':
-         # {"feature": "Total_Amount", "instances": 436, "metric_value": 0.3528, "depth": 3}
-         if obj[1]>1003967.3752647843:
-            return 'Closed Lost'
-         elif obj[1]<=1003967.3752647843:
+         # {"feature": "Delivery_Year", "instances": 392, "metric_value": 0.3423, "depth": 3}
+         if obj[2]<=2018:
+            # {"feature": "Delivery_Quarter", "instances": 345, "metric_value": 0.3751, "depth": 4}
+            if obj[3] == 'Q3':
+               return 'Closed Lost'
+            elif obj[3] == 'Q2':
+               return 'Closed Lost'
+            elif obj[3] == 'Q4':
+               return 'Closed Lost'
+            elif obj[3] == 'Q1':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
+         elif obj[2]>2018:
             return 'Closed Lost'
          else:
             return 'Closed Lost'
       elif obj[0] == 'Japan':
-         # {"feature": "Total_Amount", "instances": 51, "metric_value": 0.6723, "depth": 3}
-         if obj[1]>1003967.3752647843:
-            return 'Closed Lost'
-         elif obj[1]<=1003967.3752647843:
+         # {"feature": "Delivery_Year", "instances": 48, "metric_value": 0.7766, "depth": 3}
+         if obj[2]<=2018:
+            # {"feature": "Delivery_Quarter", "instances": 44, "metric_value": 0.8113, "depth": 4}
+            if obj[3] == 'Q1':
+               return 'Closed Lost'
+            elif obj[3] == 'Q2':
+               return 'Closed Lost'
+            elif obj[3] == 'Q3':
+               return 'Closed Lost'
+            elif obj[3] == 'Q4':
+               return 'Closed Lost'
+            else:
+               return 'Closed Lost'
+         elif obj[2]>2018:
             return 'Closed Lost'
          else:
             return 'Closed Lost'
